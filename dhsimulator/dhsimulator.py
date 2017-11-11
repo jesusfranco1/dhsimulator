b6dfddfa4d2beaa8bee131d0c48d9fb8a687c3eb
Jesus <jofranco@bu.edu>
2017-11-10 20:36:07 -0500

    deleted threading

--- dhsimulator/dhsimulator.py
+++ dhsimulator/dhsimulator.py
@@ -1,13 +1,14 @@
 import flask
 from flask import Flask, Response, render_template, request, redirect, flash, url_for
 from flaskext.mysql import MySQL
-import os, base64, datetime
+from numpy.random import uniform
 import flask_login
 from flask_login import current_user
 import time
 import random
 import logging
 import threading
+
 
 '''initiallize flask'''
 app = Flask(__name__) # create the application instance :)
@@ -25,26 +26,37 @@ def idle():
     return render_template('index.html')
 
 '''User decides to simulate bay state dinning hall'''
-@app.route('/baystate')
+@app.route('/baystate', methods=['GET', 'POST'])
 def simulate():
-    rand = random.randint(0,10)
-    return render_template('bay.html', rand=rand)
+    crowdednessvar = 0
+    day = request.form.get("day")
+    time = int(request.form.get("time"))
+    if (day == "Tuesday" or day == "Thursday"):
+        if (time <= 8) or (time >= 19):
+            crowdednessvar = 10
+        if (time > 8) and (time <= 9.125):
+            crowdednessvar = 15
+        if (time > 9.125) and (time <= 11):
+            crowdednessvar = 19
+        if (time > 11) and (time <= 14):
+            crowdednessvar = 80
+        if (time > 14) and (time<=15.125):
+            crowdednessvar = 25
+        if (time > 15.125) and (time <= 17):
+            crowdednessvar = 10
+        if (time > 17) and (time <= 18):
+            crowdednessvar = 50
+        if (time > 18) and (time <=20):
+            crowdednessvar = 70
+        else:
+            crowdednessvar = 15
 
-'''Simulate Bay State Dinning Hall. The threads simulate the users.
-   I am trying to use statistics so that the user can determine when
-   it is usually the best time to go to the dinning hall. By best,
-   I mean the least amount of people for both breakfast times, lunch
-   times, and dinner times.
-'''
-class BayDh(threading.Thread):
-    '''Constructor for class'''
-    def __init__():
-        self.hashfn = hashfn
-        self.count = count
-        self.message = random.choice(string.ascii_uppercase)
-        super(BayDh, self).__init__()
-    def run(self):
-        return 0
-    '''simulate work/ wait time for the main dish stand'''
-    def simulate_main_dish(count):
-        return 0
+    rand = expo(5)
+    #rand = BayDh
+    return render_template('bay.html', rand=rand)
+'''asume exponential service time'''
+def expo(Lambda):
+    y = uniform()
+    fx = -1*(np.log(1-y)/Lambda)
+    return fx
+def main_dish_simulator():
